c7c0cdc4be1fcb2f81448b4f11342a716f61962f
igortereshchenko <tereshchenko.igor@gmail.com>
2018-12-04 11:45:10 +0200

    Constarint layout intro

---
+++ app/src/main/java/tereshchenko/igor/mobile/LessonLayouts/ConstraintLayoutActivity.java
@@ -0,0 +1,26 @@
+package tereshchenko.igor.mobile.LessonLayouts;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import tereshchenko.igor.mobile.R;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_constraint_layout);
+
+
+
+
+
+    }
+
+}
