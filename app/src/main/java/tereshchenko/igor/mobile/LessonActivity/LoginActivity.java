b547794a3a8a158c564099d5e2b01eaf5a5c15b4
igortereshchenko <tereshchenko.igor@gmail.com>
2018-12-07 11:51:02 +0200

    start other activity

--- app/src/main/java/tereshchenko/igor/mobile/LessonActivity/LoginActivity.java
+++ app/src/main/java/tereshchenko/igor/mobile/LessonActivity/LoginActivity.java
@@ -1,7 +1,11 @@
 package tereshchenko.igor.mobile.LessonActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import tereshchenko.igor.mobile.R;
 
@@ -11,5 +15,20 @@ public class LoginActivity  extends AppCompatActivity{
         super.onCreate(stage);
 
         setContentView(R.layout.login_layout);
+
+        Button btn  = (Button) findViewById(R.id.btnLogin);
+
+        btn.setOnClickListener(view -> {
+
+            Intent container = new Intent(this,ProfileActivity.class);
+
+            EditText txtLogin = (EditText) findViewById(R.id.txtLoginLogin);
+            EditText txtPassword = (EditText) findViewById(R.id.txtPass);
+
+            container.putExtra("login",txtLogin.getText());
+            container.putExtra("password",txtPassword.getText());
+
+            startActivity(container);
+        });
     }
 }
--- app/src/main/java/tereshchenko/igor/mobile/LessonActivity/ProfileActivity.java
+++ app/src/main/java/tereshchenko/igor/mobile/LessonActivity/ProfileActivity.java
@@ -1,4 +1,36 @@
 package tereshchenko.igor.mobile.LessonActivity;
 
-public class ProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import tereshchenko.igor.mobile.R;
+
+public class ProfileActivity  extends AppCompatActivity{
+
+    protected void onCreate(Bundle stage) {
+        super.onCreate(stage);
+
+        Intent container = getIntent();
+
+        Bundle extras = container.getExtras();
+
+//        String login = (String)extras.get("login");
+//        String password =  (String)extras.get("password");
+
+
+
+
+        setContentView(R.layout.profile_layout);
+
+        EditText txtLogin = (EditText) findViewById(R.id.txtLogin);
+        txtLogin.setText("");
+
+        EditText qwe = (EditText) findViewById(R.id.txtPass2);
+        qwe.setText("");
+
+
+    }
+
 }
